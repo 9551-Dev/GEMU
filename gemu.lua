741a733733aa7b0247de3944753c362391d5c82c
Oliver Caha <74534592+9551-Dev@users.noreply.github.com>
2022-08-07 09:21:46 +0200

    optimizations

--- gemu.lua
+++ gemu.lua
@@ -1,9 +1,23 @@
+--[[
+    * api for easy interaction with drawing characters
+
+    * single file implementation of GuiH pixelbox api
+]]
+
 local EXPECT = require("cc.expect").expect
+
 local PIXELBOX = {}
 local OBJECT = {}
 local api = {}
 local ALGO = {}
 local graphic = {}
+
+local CEIL  = math.ceil
+local FLOOR = math.floor
+local SQRT  = math.sqrt
+local MIN   = math.min
+local ABS   = math.abs
+local t_insert, t_unpack, t_sort, s_char, pairs = table.insert, table.unpack, table.sort, string.char, pairs
 
 local chars = "0123456789abcdef"
 graphic.to_blit = {}
@@ -12,6 +26,7 @@ for i = 0, 15 do
     graphic.to_blit[2^i] = chars:sub(i + 1, i + 1)
     graphic.logify [2^i] = i
 end
+
 
 function PIXELBOX.INDEX_SYMBOL_CORDINATION(tbl,x,y,val)
     tbl[x+y*2-2] = val
@@ -46,14 +61,17 @@ function OBJECT:push_updates()
     PIXELBOX.ASSERT(type(self)=="table","Please use \":\" when running this function")
     self.symbols = api.createNDarray(2)
     self.lines = api.create_blit_array(self.height)
+    self.pixels = api.createNDarray(1)
     local SYMBOL_COLORS = api.createNDarray(1)
     local SYMBOL_LUT =    api.createNDarray(2)
     getmetatable(self.symbols).__tostring=function() return "PixelBOX.SYMBOL_BUFFER" end
     setmetatable(self.lines,{__tostring=function() return "PixelBOX.LINE_BUFFER" end})
-    for y,x_list in pairs(self.CANVAS) do
-        for x,block_color in pairs(x_list) do
-            local RELATIVE_X = math.ceil(x/2)
-            local RELATIVE_Y = math.ceil(y/3)
+    for y=1,#self.CANVAS do
+        local x_list = self.CANVAS[y]
+        for x=1,#x_list do
+            local block_color = x_list[x]
+            local RELATIVE_X = CEIL(x/2)
+            local RELATIVE_Y = CEIL(y/3)
             if self.UPDATES[RELATIVE_Y][RELATIVE_X] then
                 local SYMBOL_POS_X = (x-1)%2+1
                 local SYMBOL_POS_Y = (y-1)%3+1
@@ -67,6 +85,7 @@ function OBJECT:push_updates()
                     SYMBOL_POS_X,SYMBOL_POS_Y,
                     block_color
                 )
+                self.pixels[y][x] = block_color
             end
         end
     end
@@ -146,21 +165,23 @@ function OBJECT:set_pixel(x,y,color,thiccness,base)
         thiccness = thiccness or 1
         local t_ratio = (thiccness-1)/2
         self:set_box(
-            math.ceil(x-t_ratio),
-            math.ceil(y-t_ratio),
+            CEIL(x-t_ratio),
+            CEIL(y-t_ratio),
             x+thiccness-1,y+thiccness-1,color,true
         )
     else
-        local RELATIVE_X = math.ceil(x/2)
-        local RELATIVE_Y = math.ceil(y/3)
+        local RELATIVE_X = CEIL(x/2)
+        local RELATIVE_Y = CEIL(y/3)
         self.UPDATES[RELATIVE_Y][RELATIVE_X] = true
     end
 end
 
 function OBJECT:set_pixel_raw(x,y,color)
-    local RELATIVE_X = math.ceil(x/2)
-    local RELATIVE_Y = math.ceil(y/3)
-    self.UPDATES[RELATIVE_Y][RELATIVE_X] = true
+    local RELATIVE_X = CEIL(x/2)
+    local RELATIVE_Y = CEIL(y/3)
+    if not self.pixels or self.pixels[y][x] ~= color then
+        self.UPDATES[RELATIVE_Y][RELATIVE_X] = true
+    end
     self.CANVAS[y][x] = color
 end
 
@@ -176,8 +197,8 @@ function OBJECT:set_box(sx,sy,ex,ey,color,check)
     for y=sy,ey do
         for x=sx,ex do
             if self:within(x,y) then
-                local RELATIVE_X = math.ceil(x/2)
-                local RELATIVE_Y = math.ceil(y/3)
+                local RELATIVE_X = CEIL(x/2)
+                local RELATIVE_Y = CEIL(y/3)
                 self.UPDATES[RELATIVE_Y][RELATIVE_X] = true
                 self.CANVAS[y][x] = color
             end
@@ -202,8 +223,8 @@ function OBJECT:set_ellipse(x,y,rx,ry,color,filled,thiccness,check)
     for _,point in ipairs(points) do
         if self:within(point.x,point.y) then
             self:set_box(
-                math.ceil(point.x-t_ratio),
-                math.ceil(point.y-t_ratio),
+                CEIL(point.x-t_ratio),
+                CEIL(point.y-t_ratio),
                 point.x+thiccness-1,point.y+thiccness-1,color,true
             )
         end
@@ -247,8 +268,8 @@ function OBJECT:set_triangle(x1,y1,x2,y2,x3,y3,color,filled,thiccness)
     for _,point in ipairs(points) do
         if self:within(point.x,point.y) then
             self:set_box(
-                math.ceil(point.x-t_ratio),
-                math.ceil(point.y-t_ratio),
+                CEIL(point.x-t_ratio),
+                CEIL(point.y-t_ratio),
                 point.x+thiccness-1,point.y+thiccness-1,color,true
             )
         end
@@ -268,8 +289,8 @@ function OBJECT:set_line(x1,y1,x2,y2,color,thiccness)
     for _,point in ipairs(points) do
         if self:within(point.x,point.y) then
             self:set_box(
-                math.ceil(point.x-t_ratio),
-                math.ceil(point.y-t_ratio),
+                CEIL(point.x-t_ratio),
+                CEIL(point.y-t_ratio),
                 point.x+thiccness-1,point.y+thiccness-1,color,true
             )
         end
@@ -299,7 +320,7 @@ function PIXELBOX.ATTACH_GEMU(terminal,box)
     local w,h = terminal.getSize()
     for xy=1,w*h*6 do
         local x = (xy-1)%(w*2)+1
-        local y = math.ceil(xy/w*2)
+        local y = CEIL(xy/w*2)
         PIXELS[1][y][x] = colors.black
         PIXELS[2][y][x] = 15
     end
@@ -340,7 +361,7 @@ function PIXELBOX.ATTACH_GEMU(terminal,box)
             end
             if _MODE_ == 0 or _MODE_ == 1 then
                 PALETTE[0][color] = rgb
-                PALETTE[2][math.log(color,2)] = rgb
+                PALETTE[2][graphic.logify[color]] = rgb
             elseif _MODE_ == 2 then
                 if color >= 0 and color <= 255 then
                     if color >= 0 and color <= 15 then
@@ -354,8 +375,10 @@ function PIXELBOX.ATTACH_GEMU(terminal,box)
             api.update_palette(SETPALETTE, PALETTE[0])
         end
         function terminal.getPaletteColor(color)
-            return table.unpack(PALETTE[(_MODE_ == 0 or _MODE_ == 1) and 0 or 2][color])
+            return table.unpack(PALETTE[_MODE_][color] or {0,0,0})
         end
+        terminal.setPaletteColour = terminal.setPaletteColor
+        terminal.getPaletteColor  = terminal.getPaletteColor
         function terminal.setFrozen(frozen)
             if _FROZEN_ and frozen == false then
                 api.update(BOX)
@@ -378,7 +401,7 @@ function PIXELBOX.ATTACH_GEMU(terminal,box)
                 local c = api.get_closest_color(PALETTE[1],rgb)
                 BOX:set_pixel_raw(x+1,y+1,c)
             else return end
-            PIXELS[_MODE_][math.floor(y+1)][math.floor(x+1)] = color
+            PIXELS[_MODE_][FLOOR(y+1)][FLOOR(x+1)] = color
             if not _FROZEN_ and _MODE_ > 0 then
                 api.update(BOX)
             end
@@ -408,8 +431,8 @@ function PIXELBOX.ATTACH_GEMU(terminal,box)
             end
             return setmetatable(product,{})
         end
+        local CLUT = {}
         function terminal.drawPixels(x,y,pixels,w,h)
-            local CLUT = {}
             for line_index,line in ipairs(pixels) do
                 if h and line_index > h then break end
                 if type(line) == "string" then
@@ -424,10 +447,11 @@ function PIXELBOX.ATTACH_GEMU(terminal,box)
                         else
                             BOX:set_pixel_raw(x+i,y+line_index,CLUT[rgb])
                         end
-                        PIXELS[_MODE_][math.floor(x+i)][math.floor(y+line_index)] = CLUT[rgb]
+                        PIXELS[_MODE_][FLOOR(x+i)][FLOOR(y+line_index)] = CLUT[rgb]
                     end
                 elseif type(line) == "table" then
-                    for x_index,color in ipairs(line) do
+                    for x_index=1,#line do
+                        local color = line[x_index]
                         if w and x_index > w then break end
                         if _MODE_ == 0 or _MODE_ == 1 then
                             BOX:set_pixel_raw(x+x_index,y+line_index,color)
@@ -446,6 +470,9 @@ function PIXELBOX.ATTACH_GEMU(terminal,box)
             if not _FROZEN_ and _MODE_ > 0 then
                 api.update(BOX)
             end
+        end
+        function terminal.draw()
+            api.update(BOX)
         end
         function terminal.getFrozen()
             return _FROZEN_
@@ -611,7 +638,7 @@ function PIXELBOX.new(terminal,bg,existing)
 end
 
 function ALGO.get_elipse_points(radius_x,radius_y,xc,yc,filled)
-    local rx,ry = math.ceil(math.floor(radius_x-0.5)/2),math.ceil(math.floor(radius_y-0.5)/2)
+    local rx,ry = CEIL(FLOOR(radius_x-0.5)/2),CEIL(FLOOR(radius_y-0.5)/2)
     local x,y=0,ry
     local d1 = ((ry * ry) - (rx * rx * ry) + (0.25 * rx * rx))
     local dx = 2*ry^2*x
@@ -667,38 +694,43 @@ function ALGO.get_elipse_points(radius_x,radius_y,xc,yc,filled)
 end
 
 local function drawFlatTopTriangle(points,vec1,vec2,vec3)
+    local n = #points
     local m1 = (vec3.x - vec1.x) / (vec3.y - vec1.y)
     local m2 = (vec3.x - vec2.x) / (vec3.y - vec2.y)
-    local yStart = math.ceil(vec1.y - 0.5)
-    local yEnd =   math.ceil(vec3.y - 0.5)-1
+    local yStart = CEIL(vec1.y - 0.5)
+    local yEnd =   CEIL(vec3.y - 0.5)-1
     for y = yStart, yEnd do
         local px1 = m1 * (y + 0.5 - vec1.y) + vec1.x
         local px2 = m2 * (y + 0.5 - vec2.y) + vec2.x
-        local xStart = math.ceil(px1 - 0.5)
-        local xEnd =   math.ceil(px2 - 0.5)
+        local xStart = CEIL(px1 - 0.5)
+        local xEnd =   CEIL(px2 - 0.5)
         for x=xStart,xEnd do
-            table.insert(points,{x=x,y=y})
+            n = n + 1
+            points[n] = {x=x,y=y}
         end
     end
 end
 
 local function drawFlatBottomTriangle(points,vec1,vec2,vec3)
+    local n = #points
     local m1 = (vec2.x - vec1.x) / (vec2.y - vec1.y)
     local m2 = (vec3.x - vec1.x) / (vec3.y - vec1.y)
-    local yStart = math.ceil(vec1.y-0.5)
-    local yEnd =   math.ceil(vec3.y-0.5)-1
+    local yStart = CEIL(vec1.y-0.5)
+    local yEnd =   CEIL(vec3.y-0.5)-1
     for y = yStart, yEnd do
         local px1 = m1 * (y + 0.5 - vec1.y) + vec1.x
         local px2 = m2 * (y + 0.5 - vec1.y) + vec1.x
-        local xStart = math.ceil(px1 - 0.5)
-        local xEnd =   math.ceil(px2 - 0.5)
+        local xStart = CEIL(px1 - 0.5)
+        local xEnd =   CEIL(px2 - 0.5)
         for x=xStart,xEnd do
-            table.insert(points,{x=x,y=y})
+            n = n + 1
+            points[n] = {x=x,y=y}
         end
     end
 end
 function ALGO.get_triangle_points(pv1,pv2,pv3)
     local points = {}
+    local n = 0
     if pv2.y < pv1.y then pv1,pv2 = pv2,pv1 end
     if pv3.y < pv2.y then pv2,pv3 = pv3,pv2 end
     if pv2.y < pv1.y then pv1,pv2 = pv2,pv1 end
@@ -724,31 +756,35 @@ function ALGO.get_triangle_points(pv1,pv2,pv3)
     return points
 end
 function ALGO.get_line_points(startX, startY, endX, endY)
+    local n = 1
     local points = {}
-    startX,startY,endX,endY = math.floor(startX),math.floor(startY),math.floor(endX),math.floor(endY)
+    startX,startY,endX,endY = FLOOR(startX),FLOOR(startY),FLOOR(endX),FLOOR(endY)
     if startX == endX and startY == endY then return {x=startX,y=startY} end
-    local minX = math.min(startX, endX)
+    local minX = MIN(startX, endX)
     local maxX, minY, maxY
     if minX == startX then minY,maxX,maxY = startY,endX,endY
     else minY,maxX,maxY = endY,startX,startY end
     local xDiff,yDiff = maxX - minX,maxY - minY
-    if xDiff > math.abs(yDiff) then
+    if xDiff > ABS(yDiff) then
         local y = minY
         local dy = yDiff / xDiff
         for x = minX, maxX do
-            table.insert(points,{x=x,y=math.floor(y + 0.5)})
+            points[n] = {x=x,y=FLOOR(y + 0.5)}
+            n = n + 1
             y = y + dy
         end
     else
         local x,dx = minX,xDiff / yDiff
         if maxY >= minY then
             for y = minY, maxY do
-                table.insert(points,{x=math.floor(x + 0.5),y=y})
+                points[n] = points,{x=FLOOR(x + 0.5),y=y}
+                n = n + 1
                 x = x + dx
             end
         else
             for y = minY, maxY, -1 do
-                table.insert(points,{x=math.floor(x + 0.5),y=y})
+                points[n] = points,{x=FLOOR(x + 0.5),y=y}
+                n = n + 1
                 x = x - dx
             end
         end
@@ -788,21 +824,24 @@ function api.create_byte_array(count)
 end
 function api.merge_tables(...)
     local out = {}
+    local n = 0
     for k,v in pairs({...}) do
-        for _k,_v in pairs(v) do table.insert(out,_v) end
+        for _k,_v in pairs(v) do out[n] = v n=n+1 end
     end
     return out
 end
 function api.get_closest_color(palette,c)
     local result = {}
+    local n = 0
     for k,v in pairs(palette) do
-        table.insert(result,{
-            dist=math.sqrt(
+        n=n+1
+        result[n] = {
+            dist=SQRT(
                 (v[1]-c[1])^2 +
                 (v[2]-c[2])^2 +
                 (v[3]-c[3])^2
             ),  color=k
-        })
+        }
     end
     table.sort(result,function(a,b) return a.dist < b.dist end)
     return result[1].color
@@ -811,8 +850,8 @@ function api.convert_color_255(r,g,b)
     return r*255,g*255,b*255
 end
 function api.hex_to_palette(hex)
-    local r = (math.floor(hex/0x10000)%256)/255
-    local g = (math.floor(hex/0x100)%256)/255
+    local r = (FLOOR(hex/0x10000)%256)/255
+    local g = (FLOOR(hex/0x100)%256)/255
     local b = (hex%256)/255
     return r,g,b
 end
@@ -825,35 +864,47 @@ function api.update(box)
     box:push_updates()
     box:draw()
 end
-function graphic.build_drawing_char(arr,mode)
+
+local BUILDS = {}
+local count_sort = function(a,b) return a.count > b.count end
+function graphic.build_drawing_char(arr)
     local cols,fin,char,visited = {},{},{},{}
     local entries = 0
-    for k,v in pairs(arr) do
-        cols[v] = cols[v] ~= nil and
-            {count=cols[v].count+1,c=cols[v].c}
-            or (function() entries = entries + 1 return {count=1,c=v} end)()
-    end
-    for k,v in pairs(cols) do
-        if not visited[v.c] then
-            visited[v.c] = true
-            if entries == 1 then table.insert(fin,v) end
-            table.insert(fin,v)
+    local build_id = ""
+    for k = 1, 6 do
+        build_id = build_id .. ("%x"):format(graphic.logify[arr[k]])
+        if cols[arr[k]] == nil then
+            entries = entries + 1
+            cols[arr[k]] = {count=1,c=arr[k]}
+        else cols[arr[k]] = {count=cols[arr[k]].count+1,c=cols[arr[k]].c}
         end
     end
-    table.sort(fin,function(a,b) return a.count > b.count end)
-    local swap = true
-    for k=1,6 do
-        if arr[k] == fin[1].c then char[k] = 1
-        elseif arr[k] == fin[2].c then char[k] = 0
-        else char[k] =(function()
-            swap = not swap
-            return swap and 1 or 0
-        end)() end
+    if not BUILDS[build_id] then
+        for k,v in pairs(cols) do
+            if not visited[v.c] then
+                visited[v.c] = true
+                if entries == 1 then t_insert(fin,v) end
+                t_insert(fin,v)
+            end
+        end
+        t_sort(fin, count_sort)
+        local swap = true
+        for k=1,6 do
+            if arr[k] == fin[1].c then char[k] = 1
+            elseif arr[k] == fin[2].c then char[k] = 0
+            else
+                swap = not swap
+                char[k] = swap and 1 or 0
+            end
+        end
+        if char[6] == 1 then for i = 1, 5 do char[i] = 1-char[i] end end
+        local n = 128
+        for i = 0, 4 do n = n + char[i+1]*2^i end
+        if char[6] == 1 then BUILDS[build_id] = {s_char(n), fin[2].c, fin[1].c}
+        else BUILDS[build_id] = {s_char(n), fin[1].c, fin[2].c}
+        end
     end
-    if char[6] == 1 then for i = 1, 5 do char[i] = 1-char[i] end end
-    local n = 128
-    for i = 0, 4 do n = n + char[i+1]*2^i end
-    return string.char(n),char[6] == 1 and fin[2].c or fin[1].c,char[6] == 1 and fin[1].c or fin[2].c
+    return t_unpack(BUILDS[build_id])
 end
 
 return {
